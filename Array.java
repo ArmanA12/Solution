c1bf6bb37f72fd180a4f5695283a6204689e86e4
Arman shekh <mdarmanshekh4@gmail.com>
2022-05-03 21:33:57 +0530

    arman

--- Array.java
+++ Array.java
@@ -1,34 +1,22 @@
 import java.util.*;
 public class Array{
-    static  void  Rotate(int [] array, int k  ){
-          if(k == array.length){
-              return;
-          }
-        while(k > 0){
-            int temp  = array[array.length-1];
-        for(int i = array.length-2; i>=0; i--){
-               int temp1 = array[i];
-               array[i+1] = temp1;
+    static int Solution(int position, int target, int distance){
+        if(position == target){
+            return 0;
         }
-        array[0] = temp;
-        k--;
-    
-      }
+        int jump = 0;
+        while ( position <= target){
+            position += distance;
+            jump++;
+        }
+        return jump;
     }
-
    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the length of array");
-        int n = sc.nextInt();
-        int []  array = new int[n];
-        for(int i =0; i<array.length; i++){
-            array[i] = sc.nextInt();
-        }
-        System.out.println("enter the value of k");
-        int k = sc.nextInt();
-        Rotate(array, k);
-        for(int i =0; i<array.length; i++){
-            System.out.print(array[i] + " ");
-        }
-    }
+        int position = sc.nextInt();
+        int target = sc.nextInt();
+        int distance = sc.nextInt();
+        System.out.println(Solution(position, target, distance));
+   }
+     
 }
